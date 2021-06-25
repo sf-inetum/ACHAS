3c6a75b459fbfc22784f240b718727226ddde57c
Sergio Gregorio Baca Morales <sergio-gregorio.baca@gfi.world>
2021-06-25 09:28:08 +0200

    Testing Deploy

---
+++ .sfdx/typings/lwc/apex/ACHSAccountForm.d.ts
@@ -0,0 +1,12 @@
+declare module "@salesforce/apex/ACHSAccountForm.getMDTpermission" {
+  export default function getMDTpermission(param: {rtIdAcc: any, inUserId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/ACHSAccountForm.getRTdefault" {
+  export default function getRTdefault(): Promise<any>;
+}
+declare module "@salesforce/apex/ACHSAccountForm.getRecordTypeAccount" {
+  export default function getRecordTypeAccount(): Promise<any>;
+}
+declare module "@salesforce/apex/ACHSAccountForm.getAccountRTs" {
+  export default function getAccountRTs(): Promise<any>;
+}
